60c225e36a2b41d61ecf720d45517da6c150b8d8
Adam Ciołkowski <adamciolkowski1@gmail.com>
2018-05-09 22:02:40 +0200

    Use initial hello world source code

--- src/main/java/jrunner/IoUtils.java
+++ src/main/java/jrunner/IoUtils.java
@@ -3,6 +3,8 @@ package jrunner;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
 
 public class IoUtils {
 
@@ -11,6 +13,14 @@ public class IoUtils {
         int read;
         while ((read = in.read(buffer)) != -1) {
             out.write(buffer, 0, read);
+        }
+    }
+
+    public static void copy(Reader reader, Writer writer) throws IOException {
+        char[] buffer = new char[4096];
+        int read;
+        while ((read = reader.read(buffer)) != -1) {
+            writer.write(buffer, 0, read);
         }
     }
 }
--- src/main/java/jrunner/web/JRunnerController.java
+++ src/main/java/jrunner/web/JRunnerController.java
@@ -1,18 +1,24 @@
 package jrunner.web;
 
+import jrunner.IoUtils;
 import jrunner.JRunnerService;
 import jrunner.Output;
 import jrunner.compiler.CompilationFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.StringWriter;
 import java.io.Writer;
-import java.nio.charset.StandardCharsets;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Controller
 public class JRunnerController {
@@ -25,8 +31,24 @@ public class JRunnerController {
     }
 
     @GetMapping("/")
-    public String get() {
+    public String get(Model model) {
+        model.addAttribute("sourceCode", initialSourceCode());
         return "index";
+    }
+
+    private String initialSourceCode() {
+        try {
+            return tryReadInitialSourceCode();
+        } catch (IOException e) {
+            return  "";
+        }
+    }
+
+    private String tryReadInitialSourceCode() throws IOException {
+        InputStream in = getClass().getResourceAsStream("/HelloWorld.java");
+        StringWriter out = new StringWriter();
+        IoUtils.copy(new InputStreamReader(in, UTF_8), out);
+        return out.toString();
     }
 
     @PostMapping("/")
@@ -34,7 +56,7 @@ public class JRunnerController {
         try {
             jRunnerService.compileAndRun(sourceCode, new Output(out, out));
         } catch (CompilationFailedException e) {
-            Writer writer = new OutputStreamWriter(out, StandardCharsets.UTF_8);
+            Writer writer = new OutputStreamWriter(out, UTF_8);
             writer.write(e.getMessage());
             writer.flush();
         }
